02dbd7ce626cf539b2343dcc5497db0d4045a042
PataiSzabolcs <pataiszabolcs@gmail.com>
2017-11-27 16:26:18 +0100

    feat(completing exercise-9)

---
+++ week-02/day-1/exercise-9/exercise-9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+
+            Console.WriteLine($"The BMI of this person is {massInKg / heightInM * heightInM}.");
+            Console.ReadLine();
+        }
+    }
+}
