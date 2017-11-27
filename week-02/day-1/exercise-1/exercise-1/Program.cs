8dca40bb0225de0f64c9e64e3fbb17df34585e3b
PataiSzabolcs <pataiszabolcs@gmail.com>
2017-11-27 15:10:41 +0100

    gitignore, week-01 day-2 changes, week-02 day-1 exercise-1

---
+++ week-02/day-1/exercise-1/exercise-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet you instead of the World!
+            Console.WriteLine("Hello, Szabolcs");
+            Console.ReadLine();
+        }
+    }
+}
