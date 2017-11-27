2b29f75b8af70f40963bc28d1ed32394ed87bf10
PataiSzabolcs <pataiszabolcs@gmail.com>
2017-11-27 18:01:46 +0100

    feat(completing exercises): exercise-18, exercise-22, exercise-30

---
+++ week-02/day-1/exercise-18/exercise-18/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            if (number % 2 == 1)
+            {
+                Console.WriteLine("Odd");
+            }
+            else
+            {
+                Console.WriteLine("Even");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-1/exercise-30/exercise-30/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // diamond like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //  *****
+            //   ***
+            //    *
+            //
+            // The diamond should have as many lines as the number was
+            Console.WriteLine(Give me a number:);
+            string input = Console.ReadLine();
+            int number = 
+
+
+        }
+    }
+}
