27fd034bc0854143edc589e225617bcf25407799
PataiSzabolcs <pataiszabolcs@gmail.com>
2017-11-29 12:06:14 +0100

    feat(create project): chess table exercise

---
+++ week-02/day-1/drawchesstable/drawchesstable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+
+            for (int i = 0; i < 8; i++)
+            {
+                if ()
+            }
+
+
+        }
+    }
+}
