3ebb88a48650f79594d68415abaabb64d3fcf4ad
PataiSzabolcs <pataiszabolcs@gmail.com>
2017-12-06 10:14:26 +0100

    feat(reversedlines)creating reversedlines solution

---
+++ week-03/day-1/ReversedLines/ReversedLines/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReversedLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a method that decrypts reversed-lines.txt
+
+            ReverseLine(@"C:\Users\Szabi\Desktop\reversed-lines.txt", @"C:\Users\Szabi\Desktop\decrypt2.txt");
+        }
+        static void ReverseLine(string sourceFilename, string targetFilename)
+        {
+            string[] lines = File.ReadAllLines(sourceFilename);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                char[] charArray = lines[i].ToCharArray();
+                string reverse = String.Empty;
+
+                for (int j = charArray.Length; j > 0; j--)
+                {
+                    reverse += charArray[j - 1];
+                }
+                lines[i] = reverse;
+            }
+            File.WriteAllLines(targetFilename, lines);            
+        }
+    }
+}
---
+++ week-03/day-1/ReversedOrder/ReversedOrder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a method that decrypts reversed-order.txt
+
+            ReverseOrder(@"C:\Users\Szabi\Desktop\reversed-order.txt", @"C:\Users\Szabi\Desktop\decrypt3.txt");
+        }
+        static void ReverseOrder(string sourceFilename, string targetFilename)
+        {
+            string[] lines = File.ReadAllLines(sourceFilename);
+            string[] reverselines = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                reverselines[i] = lines[lines.Length - 1 - i];
+            }
+            File.WriteAllLines(targetFilename, reverselines);
+        }
+    }
+}
