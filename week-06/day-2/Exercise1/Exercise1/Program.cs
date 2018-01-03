5979c5bd0394085e8c5b3542f21eeba1fa81a679
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-03 13:44:18 +0100

    feat(completing exercise-1)

---
+++ week-06/day-2/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            int[] evenNumbers = n.Where(i => i % 2 == 0).ToArray();
+            foreach (int number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
