f09b3be60aaf200deda9d9ee2fb5d1efb53214a0
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-03 15:10:03 +0100

    feat(completing exercise-6)

---
+++ week-06/day-2/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Ain't nobody got time for that!";
+
+            var characterFrequency = text.Select(c => c).GroupBy(c => c).ToDictionary(c => c.Key, c => c.Count()).OrderByDescending(c => c.Value);
+
+            foreach (var kv in characterFrequency)
+            {
+                Console.WriteLine(kv);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
