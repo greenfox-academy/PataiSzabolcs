89c26598b6e280c7d0a82642eca0911b7848f2f2
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-03 16:55:26 +0100

    feat(completing exercise-9)

---
+++ week-06/day-2/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = { 'C', 'h', 'a', 'r', ' ', 'a', 'r', 'r', 'a', 'y', ' ', 'b', 'e', 'c', 'o', 'm', 'e', 's', ' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g', '.' };
+
+
+
+            string text = String.Join("", charArray.Select(o => o.ToString()).ToArray());
+
+            Console.WriteLine(text);
+
+            Console.ReadLine();
+           
+
+        }
+    }
+}
