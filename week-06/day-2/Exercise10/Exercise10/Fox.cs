7aa11e260d1002a89fbaf2c29c9f01c67db1dbe7
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-03 17:18:10 +0100

    feat(completing exercise-10)

---
+++ week-06/day-2/Exercise10/Exercise10/Fox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Fox
+    {
+        //Create a Fox class with 3 properties(name, type, color) Fill a list with at least 5 foxes,
+        //it's up to you how you name/create them! Write a LINQ Expression to find the foxes with green color!
+        //Write a LINQ Expression to find the foxes with green color and pallida type!
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        public Fox(string name, string type, string color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+        }
+    }
+}
---
+++ week-06/day-2/Exercise10/Exercise10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fox> foxList = new List<Fox>();
+            foxList.Add(new Fox("Vuk", "Pallida", "Red"));
+            foxList.Add(new Fox("Foxy Lady", "Pallida", "Green"));
+            foxList.Add(new Fox("Roka Koma", "Corsac", "Green"));
+            foxList.Add(new Fox("Ravaszdi", "Corsac", "Green"));
+            foxList.Add(new Fox("Green Pallida Fox", "Pallida", "Green"));
+
+            foxList.Where(f => f.Type == "Pallida" && f.Color == "Green").ToList().ForEach(f => Console.WriteLine(f.Name));
+
+            Console.ReadLine();
+        }
+    }
+}
