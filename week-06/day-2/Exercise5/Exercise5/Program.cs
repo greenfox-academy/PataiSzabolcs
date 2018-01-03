afc3265af4b76a3bdc0fe5d1236c7de9d10e17a2
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-03 14:56:07 +0100

    feat(completing exercise-5)

--- week-06/day-2/Exercise5/Exercise5/Program.cs
+++ week-06/day-2/Exercise5/Exercise5/Program.cs
@@ -10,6 +10,16 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var numberFrequency = n.Select(i => i).GroupBy(i => i).ToDictionary(i => i.Key, i => i.Count());
+
+            foreach (KeyValuePair<int, int> kv in numberFrequency)
+            {
+                Console.WriteLine(kv);
+            }
+
+            Console.ReadLine();
         }
     }
 }
