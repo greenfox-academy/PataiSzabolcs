c5a11b187f6fcc748ef1a7bfa8f0ba3337d5f20c
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-03 15:16:49 +0100

    feat(completing exercise-7)

---
+++ week-06/day-2/Exercise7/Exercise7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            cities.Where(c => c.First() == 'A' && c.Last() == 'I').ToList().ForEach(c => Console.WriteLine(c));
+
+            Console.ReadLine();
+        }
+    }
+}
