ec23d7c37681dff4d9536e929090758455c7c958
PataiSzabolcs <pataiszabolcs@gmail.com>
2018-01-22 12:21:41 +0100

    feat(api): creating project, creating doubling endpoint

---
+++ week-09/day-1/Frontend/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return File("index.html", "text/html");
+        }
+
+        [HttpGet("/doubling")]
+        public IActionResult Doubling(int? input)
+        {
+            if (input != null)
+            {
+                return Json(new { received = input, result = input * 2 });
+            }
+            return Json(new { error = "Please provide an input!"});
+        }
+    }
+}
